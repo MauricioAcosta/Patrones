aaa1c242f57d5cf9f62e97cb06d3df1127f6cac6
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2018-09-27 17:46:38 -0500

    Relación de uso entre SelectedOrders y ModelOrder

--- Visitor_copy/src/SelectedOrders.java
+++ Visitor_copy/src/SelectedOrders.java
@@ -1,32 +1,31 @@
 import java.util.*;
 
 public class SelectedOrders implements Iterator {
-  //AllCandidates ac;
+  
   String selectedType;
   ModelOrder nextOrder;
   Enumeration ec;
-  AllOrders ao;
-  
-  
+  AllOrders ao;  
 
-
-  public SelectedOrders( AllOrders inp_ac, String type) {
-    ao = inp_ac;
+  public SelectedOrders(
+          //AllOrders inp_ac, 
+          String type) {
+    //ao = inp_ac;
     selectedType = type;
-    ec = inp_ac.getAllOrders();
+    //ec = inp_ac.getAllOrders();
   }
   
   public boolean hasNext() {
     boolean matchFound = false;
     while (ec.hasMoreElements()) {
-      ModelOrder tempObj = (ModelOrder) ec.nextElement();
-      if (tempObj.getOrderSeletedType().equals(
-            selectedType)) {
+      //ModelOrder tempObj = (ModelOrder) ec.nextElement();
+      //if (tempObj.getOrderSeletedType().equals(
+            //selectedType)) {
         matchFound = true;
-        nextOrder = tempObj;
+        //nextOrder = tempObj;
         break;
       }
-    }
+    //}
     if (matchFound == true) {
     } else {
       nextOrder = null;
@@ -34,14 +33,20 @@ public class SelectedOrders implements Iterator {
     return matchFound;
   }
 
- public Object next() {
+ /*public Object next() {
     if (nextOrder == null) {
       throw new NoSuchElementException();
     } else {
       return nextOrder;
     }
-  }
+  }*/
 
   public void remove() {};
 
+    @Override
+    public Object next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
+    
